0e03b634cafb02f5aaa0eaa36d180d35c97e80e2
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-26 14:23:53 +0000

    verificacao de testes

--- projeto/models/pessoa.py
+++ projeto/models/pessoa.py
@@ -2,19 +2,59 @@ from projeto.models.enums.sexo import Sexo
 
 class Pessoa:
     def __init__(self, nome: str, idade: int, sexo: Sexo) -> None:
-        self.nome = nome
-        self.idade = idade
+        self.nome = self._verificar_nome(nome)
+        self.idade = self._verificar_idade(idade)
         self.sexo = sexo
 
-    def set_idade(self, idade):
-        if idade < 0:
-            self.idade = 0
-        else:
-            self.idade = idade
+   # def _verificar_idade(self,valor):
+     # if valor >= 130:
+        # raise ValueError("Idade não pode ultrapassar o limite")
+        
+        # if valor < 0:
+        #    raise ValueError("A idade não pode ser negativa.")
+                
+    # self.idade = valor
+    # return self.idade
 
-    def __str__(self) -> str:
-        return(
-            f"\nNome: {self.nome}"
-            f"\nIdade: {self.idade}"
-            f"\nSexo: {self.sexo.value}"
-        )        
+    def _verificar_nome(self,valor):
+        """Método para verificação de nome"""
+        self._verificar_nome_tipo_invalido(valor)
+        self._verificar_nome_vazio_invalido(valor)
+
+        self.nome = valor
+        return self.nome
+
+    def _verificar_idade(self,valor):
+        """Método auxiliar para verificação de idade"""
+        self._verificar_idade_tipo_invalido(valor)
+        self._verificar_idade_negativa(valor)
+        self._verificar_idade_acima_130(valor)
+
+        self.idade = valor
+        return self.idade
+
+    def _verificar_idade_tipo_invalido(self,valor):
+        """Método auxiliar para verificação de tipo para idade"""
+        if not isinstance(valor, int):
+            raise TypeError("A idade deve ser um número inteiro")
+
+    def _verificar_idade_negativa(self,valor):
+        """Método auxiliar para verificação de idade negativa"""
+        if valor < 0:
+            raise ValueError("A idade não pode ser negativa.")
+        
+    def _verificar_idade_acima_130(self,valor):
+        """Método auxiliar para verificação da idade limite"""
+        if valor >= 130:
+            raise ValueError("Idade não pode ultrapassar o limite")
+        
+    def _verificar_nome_tipo_invalido(self,valor):
+        """Método auxiliar para verificação de tipo para nome"""
+        if not isinstance(valor, str):
+            raise TypeError("O nome deve ser um texto")
+        
+    def _verificar_nome_vazio_invalido(self,valor):
+        """Método auxiliar para verificação de nome inexistente"""
+        # strip() Verifica se o nome está vazio ou contém apenas espaços.
+        if not valor.strip():
+            raise TypeError("O nome não pode estar vazio")
--- projeto/tests/test_pessoa.py
+++ projeto/tests/test_pessoa.py
@@ -18,6 +18,22 @@ def test_pessoa_mudar_nome_valido(pessoa_valida):
 def test_pessoa_idade_valido(pessoa_valida):
     assert pessoa_valida.idade == 33
 
-def test_pessoa_idade_negativa(pessoa_valida):
-    pessoa_valida.set_idade(-1)
-    assert pessoa_valida.idade == 0
+def test_pessoa_idade_negativa():
+    with pytest.raises(ValueError, match="A idade não pode ser negativa."):
+        Pessoa("Maiara", -33, Sexo.FEMININO)
+
+def test_pessoa_idade_maior_que_130():
+    with pytest.raises(ValueError, match="Idade não pode ultrapassar o limite"):
+        Pessoa("Maiara", 130, Sexo.FEMININO)
+
+def test_pessoa_idade_tipo_invalido_retorna_mensagem_erro():
+    with pytest.raises(TypeError, match="A idade deve ser um número inteiro"):
+        Pessoa("Maiara", "33", Sexo.FEMININO)
+
+def test_pessoa_nome_tipo_invalido_retorna_mensagem_erro():
+    with pytest.raises(TypeError, match="O nome deve ser um texto"):
+        Pessoa(222, 333, Sexo.FEMININO)
+
+def test_pessoa_nome_vazio_tipo_invalido_retorna_mensagem_erro():
+    with pytest.raises(TypeError, match="O nome não pode estar vazio"):
+        Pessoa("", 333, Sexo.FEMININO)
